9c586261542cdab9718e1fc7b88df9e56da1db32
Artyom-exe <thlambot@hotmail.fr>
2025-03-18 12:14:51 +0100

    Amélioration des styles des boutons PrimaryButton et SecondaryButton : ajout de transitions et ajustement des classes CSS pour une meilleure interactivité.

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -13,7 +13,7 @@ defineProps({
 
 <template>
     <button :type="type" :class="[
-        'relative flex justify-center items-center px-6 bg-[#FF8C42] rounded-md font-[500] text-base font-inter text-white border-2 border-transparent',
+        'relative flex justify-center items-center px-6 border border-[#FF8C42] bg-[#FF8C42] rounded-md font-[500] text-base font-inter text-white',
         'transition-all duration-300 ease-in-out',
         'hover:bg-transparent hover:text-[#FF8C42] hover:border-[#FF8C42]',
         'focus:outline-none focus:bg-transparent focus:text-[#FF8C42] focus:border-[#FF8C42]',
--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -8,7 +8,12 @@ defineProps({
 </script>
 
 <template>
-    <button :type="type" class="flex justify-center items-center px-6 py-[10px] border border-white/20 rounded-md font-[500] text-base font-inter text-white">
+    <button :type="type" :class="[
+        'flex justify-center items-center px-6 py-[10px] border border-white/20 rounded-md font-[500] text-base font-inter text-white',
+        'transition-all duration-300 ease-in-out',
+        'hover:bg-white/20',
+        'focus:outline-none focus:bg-white/20'
+    ]">
         <slot />
     </button>
 </template>
