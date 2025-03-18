c31f5c1b41f2ea5e2109c253f6972fc3781402c2
Artyom-exe <th.lambot@hotmail.fr>
2025-03-18 14:44:07 +0100

    Ajout du modèle Service, du contrôleur ServiceController, des migrations et des seeders pour gérer les services. Inclusion de plusieurs icônes SVG pour représenter les services.

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json(Service::all());
+    }
+}
---
+++ database/seeders/ServiceSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Service;
+use Illuminate\Database\Seeder;
+
+class ServiceSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $services = [
+            [
+                'title' => 'Panneaux photovoltaïques',
+                'icon' => 'assets/icons/services/solar-panel-solid.svg',
+                'short_description' => 'Installation de panneaux solaires pour une énergie renouvelable.',
+                'description' => 'Nous vous accompagnons dans l’installation et la maintenance de vos panneaux photovoltaïques pour optimiser votre consommation d’énergie verte.',
+                'image' => 'images/services/install-solare-panel.jpg',
+            ],
+            [
+                'title' => 'Bornes de recharge pour véhicule',
+                'icon' => 'assets/icons/services/charging-station-solid.svg',
+                'short_description' => 'Installation de bornes de recharge pour voitures électriques.',
+                'description' => 'Nous proposons des solutions adaptées pour la recharge de véhicules électriques, en résidentiel ou en entreprise.',
+                'image' => 'images/services/charging-station.jpg',
+            ],
+            [
+                'title' => 'Mise en conformité électrique',
+                'icon' => 'assets/icons/services/bolt-solid.svg',
+                'short_description' => 'Mise en conformité des installations électriques.',
+                'description' => 'Nous assurons l’audit et la mise en conformité des installations électriques selon les normes en vigueur pour garantir votre sécurité et votre conformité réglementaire.',
+                'image' => 'images/services/electrical-panel.jpg',
+            ],
+            [
+                'title' => 'Gros œuvre électrique',
+                'icon' => 'assets/icons/services/screwdriver-wrench-solid.svg',
+                'short_description' => 'Installation des infrastructures électriques de base.',
+                'description' => 'Nous réalisons le câblage et la mise en place des infrastructures électriques dès la phase de construction ou de rénovation pour assurer une distribution optimale de l’électricité.',
+                'image' => 'images/services/electrical-construction-site.jpg',
+            ],
+        ];
+
+        foreach ($services as $service) {
+            Service::updateOrCreate(
+                ['title' => $service['title']],
+                $service
+            );
+        }
+    }
+}
