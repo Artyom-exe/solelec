33b3892f4d04279d7a178aabb846266a63bf13c0
Artyom-exe <thlambot@hotmail.fr>
2025-03-31 10:31:08 +0200

    Amélioration du composant ReviewSlider avec un affichage de 4 avis par page et ajustement du style pour une meilleure présentation. Ajout d'une section "Témoignages clients" sur la page d'accueil.

--- resources/js/Components/ReviewSlider.vue
+++ resources/js/Components/ReviewSlider.vue
@@ -16,7 +16,8 @@ onMounted(async () => {
   <Splide
     :options="{
       type: 'loop',
-      perPage: 1,
+      perPage: 4,
+      gap: '32px',
       arrows: false,
       pagination: false,
       drag: false,
@@ -25,10 +26,10 @@ onMounted(async () => {
       },
     }"
     :extensions="{ AutoScroll }"
-    class="max-w-xl mx-auto"
+    class="w-full"
   >
     <SplideSlide v-for="review in reviews" :key="review.id">
-      <div class="p-6 bg-white shadow rounded-lg">
+      <div class="max-w-[416px] h-[233px] flex p-8 flex-col gap-6 rounded-lg border border-white/20 bg-[#242424] text-white">
         <p class="text-yellow-500 font-bold text-lg">★ {{ review.note }}/5</p>
         <p class="italic text-gray-700">"{{ review.comment }}"</p>
         <p class="mt-2 text-sm text-gray-500">— {{ review.author }}</p>
