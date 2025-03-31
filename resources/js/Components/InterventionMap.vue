027e0474a661b8bebad911e5c1541936aaa2ddc2
Artyom-exe <thlambot@hotmail.fr>
2025-03-31 16:59:50 +0200

    Amélioration du composant InterventionMap avec des ajustements de style et d'animation. Correction de balises HTML et ajout d'attributs pour une meilleure accessibilité.

--- resources/js/Components/InterventionMap.vue
+++ resources/js/Components/InterventionMap.vue
@@ -46,37 +46,42 @@ onMounted(() => {
 </script>
 
 <template>
-  <artic class="flex flex-col gap-8">
+  <article class="flex flex-col gap-8 py-16 px-4 md:px-8 lg:px-16 bg-[#F5F5F5]" data-aos="fade-up" data-aos-duration="800">
+
     <div class="flex flex-col md:flex-row gap-8">
       <!-- Deux colonnes -->
-    <div class="grid grid-cols-2 gap-6 md:w-1/2">
+      <div class="grid grid-cols-2 gap-6 md:w-1/2" data-aos="fade-right" data-aos-delay="100" data-aos-duration="800">
         <button
-            v-for="city in cities"
-            :key="city.name"
-            @click="centerOn(city.coords)"
-            class="group flex flex-col items-start gap-2 px-6 py-5 rounded-lg border border-transparent transition-all duration-300 ease-in-out hover:bg-[#f9f9f9] hover:border-[#FF8C42] hover:shadow-md"
+          v-for="(city, index) in cities"
+          :key="city.name"
+          @click="centerOn(city.coords)"
+          class="group flex flex-col items-start gap-2 px-6 py-5 rounded-lg border border-transparent transition-all duration-300 ease-in-out hover:bg-[#f9f9f9] hover:border-[#FF8C42] hover:shadow-md"
+          :data-aos="index % 2 === 0 ? 'fade-up' : 'fade-down'"
+          :data-aos-delay="150 + (index * 50)"
+          data-aos-duration="600"
+          data-aos-easing="ease-in-out"
         >
-            <h3 class="text-left text-xl font-semibold text-[#0D0703] group-hover:text-[#FF8C42] transition-colors duration-300 font-poppins">
+          <h3 class="text-left text-xl font-semibold text-[#0D0703] group-hover:text-[#FF8C42] transition-colors duration-300 font-poppins">
             {{ city.name }}
-            </h3>
-            <div class="flex gap-3 items-center">
-                <p class="text-left text-sm font-medium text-gray-500 group-hover:text-[#0D0703] font-inter">
-                Voir sur la carte
-                </p>
-                <svg
-                class="w-4 h-4 transition-transform duration-300 group-hover:translate-x-1 text-[#0D0703] group-hover:text-[#FF8C42]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-                fill="currentColor"
-                >
-                <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
-                </svg>
-            </div>
+          </h3>
+          <div class="flex gap-3 items-center">
+            <p class="text-left text-sm font-medium text-gray-500 group-hover:text-[#0D0703] font-inter">
+              Voir sur la carte
+            </p>
+            <svg
+              class="w-4 h-4 transition-transform duration-300 group-hover:translate-x-1 text-gray-500 group-hover:text-[#FF8C42]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              fill="currentColor"
+            >
+              <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
+            </svg>
+          </div>
         </button>
-    </div>
+      </div>
 
       <!-- Carte -->
-      <div class="md:w-1/2 h-[440px] rounded-lg overflow-hidden shadow">
+      <div class="md:w-1/2 h-[440px] rounded-lg overflow-hidden shadow" data-aos="zoom-in" data-aos-delay="300" data-aos-duration="1000">
         <div ref="mapElement" class="w-full h-full"></div>
       </div>
     </div>
@@ -85,7 +90,7 @@ onMounted(() => {
     <p class="sr-only">
       Solelec intervient en Wallonie et à Bruxelles, notamment à Ottignies, Louvain-la-Neuve, Wavre, Nivelles, Gembloux,
       Namur, Charleroi, Liège, Mons, Rixensart, Court-Saint-Étienne, Mont-Saint-Guibert, Eghezée, Sombreffe, Perwez,
-      Bastogne, Spa, Marche-en-Famenne, Tournai et bien d’autres localités.
+      Bastogne, Spa, Marche-en-Famenne, Tournai et bien d'autres localités.
     </p>
-  </artic>
+  </article>
 </template>
