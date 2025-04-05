90a0a2c96c63608fa677c73d439a37e4109edb10
Artyom-exe <thlambot@hotmail.fr>
2025-04-05 20:02:41 +0200

    Ajout du composant logo et intégration dans NavBarPublic et PublicLayout. Mise à jour de la structure de la page avec un footer contenant des informations de contact et de navigation.

---
+++ resources/js/Components/logo.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Link href="/" class="flex items-center">
+        <span class="font-inter font-extrabold text-xl text-white leading-7">S<span class="text-[#FF8C42]">O</span>LELEC</span>
+    </Link>
+</template>
