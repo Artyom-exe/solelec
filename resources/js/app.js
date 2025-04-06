f60d6fd995a7dc49e583ea921c49d8b6849aec91
Artyom-exe <thlambot@hotmail.fr>
2025-04-06 19:31:52 +0200

    Ajout de la dépendance vue-final-modal et intégration dans app.js. Mise à jour de package.json et package-lock.json pour inclure les nouvelles dépendances.

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import '@splidejs/vue-splide/css';
+import { createVfm } from 'vue-final-modal'
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,9 +15,12 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
+        const vfm = createVfm()
+
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(vfm)
             .mount(el);
     },
     progress: {
