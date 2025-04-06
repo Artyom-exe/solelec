2db32fd92a1b6e07175ed69b2847f871fedf618c
Artyom-exe <thlambot@hotmail.fr>
2025-04-06 22:40:55 +0200

    Ajout du composant ModalConfirm et intégration de la gestion des modales dans PublicLayout. Mise à jour de app.js pour inclure les styles et la configuration de vue-final-modal.

---
+++ resources/js/Components/ModalConfirm.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { VueFinalModal } from 'vue-final-modal'
+
+defineProps<{
+  title?: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'confirm'): void
+}>()
+</script>
+
+<template>
+  <VueFinalModal
+    overlay-transition="vfm-fade"
+    content-transition="vfm-scale-fade"
+    overlay-class="custom-overlay"
+    class="flex justify-center items-center"
+    content-class="flex flex-col max-w-xl mx-4 p-4 bg-white dark:bg-gray-900 border dark:border-gray-700 rounded-lg space-y-2"
+  >
+    <h1 class="text-xl">
+      {{ title }}
+    </h1>
+    <slot />
+    <button class="mt-1 ml-auto px-2 border rounded-lg" @click="emit('confirm')">
+      Confirm
+    </button>
+  </VueFinalModal>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,9 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import '@splidejs/vue-splide/css';
-import { createVfm } from 'vue-final-modal'
+import { createVfm } from 'vue-final-modal';
+import 'vue-final-modal/style.css';
+
 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
