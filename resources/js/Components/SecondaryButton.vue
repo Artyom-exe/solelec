afe01a9704acf70a752e5c30744dccc8cbc88c29
Artyom-exe <th.lambot@hotmail.fr>
2025-04-08 12:34:43 +0200

    Refactor des composants PrimaryButton et SecondaryButton : amélioration de la structure du code, ajout de la gestion du focus et uniformisation des styles.

--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -1,45 +1,56 @@
 <script setup>
-import { router } from '@inertiajs/vue3'
+import { router } from "@inertiajs/vue3";
 
 const props = defineProps({
-  type: {
-    type: String,
-    default: 'button',
-  },
-  variant: {
-    type: String,
-    default: 'default', // 'default' ou 'dark'
-  },
-  to: {
-    type: String,
-    default: null,
-  },
+    type: {
+        type: String,
+        default: "button",
+    },
+    variant: {
+        type: String,
+        default: "default", // 'default' ou 'dark'
+    },
+    to: {
+        type: String,
+        default: null,
+    },
 });
 
-const handleClick = () => {
-  if (props.to) {
-    const isExternal = props.to.startsWith('http')
-    if (isExternal) {
-      window.open(props.to, '_blank')
-    } else {
-      router.visit(props.to)
+const handleClick = (event) => {
+    const button = event.currentTarget;
+    button.blur();
+
+    if (props.to) {
+        const isExternal = props.to.startsWith("http");
+        if (isExternal) {
+            window.open(props.to, "_blank");
+        } else {
+            router.visit(props.to);
+        }
     }
-  }
 };
 </script>
 
 <template>
-  <button
-    :type="type"
-    :class="[
-      'flex justify-center items-center px-6 py-[10px] rounded-md font-[500] text-base font-inter',
-      'transition-all duration-300 ease-in-out',
-      variant === 'default'
-        ? ['border border-white/20 text-white', 'hover:bg-white/20', 'focus:outline-none focus:bg-white/20']
-        : ['border border-[#0D070326/15] text-[#0D0703]', 'hover:bg-gray-200', 'focus:outline-none focus:bg-gray-100']
-    ]"
-    @click="handleClick"
-  >
-    <slot />
-  </button>
+    <button
+        :type="type"
+        :class="[
+            'flex justify-center items-center px-6 py-[10px] rounded-md font-[500] text-base font-inter',
+            'transition-all duration-300 ease-in-out',
+            variant === 'default'
+                ? [
+                      'border border-white/20 text-white',
+                      'hover:bg-white/20',
+                      'focus:outline-none focus:bg-white/20',
+                  ]
+                : [
+                      'border border-[#0D070326/15] text-[#0D0703]',
+                      'hover:bg-gray-200',
+                      'focus:outline-none focus:bg-gray-100',
+                  ],
+        ]"
+        @click="handleClick"
+    >
+        <slot />
+    </button>
 </template>
