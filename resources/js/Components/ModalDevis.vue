ececc4e95cd38d0da4bae58a06b5c80bff26bdbe
Artyom-exe <th.lambot@hotmail.fr>
2025-04-08 09:41:31 +0200

    Refactor du composant ModalDevis : amélioration de la structure du code, ajustement des styles et mise à jour des éléments de l'interface pour une meilleure expérience utilisateur.

--- resources/js/Components/ModalDevis.vue
+++ resources/js/Components/ModalDevis.vue
@@ -18,30 +18,25 @@ const emit = defineEmits<{
     (e: "submit", formData: Record<string, any>): void;
 }>();
 
-// Étape courante (1 : services, 2 : description, 3 : coordonnées)
 const step = ref(1);
-
-// Services sélectionnés (IDs)
 const selectedServiceIds = ref<number[]>([]);
 
-// Données du formulaire regroupant les infos pour le devis et le client
 const formData = reactive({
-    services: [] as number[], // tableau des IDs de services sélectionnés
+    services: [] as number[],
     description: "",
-    desiredDate: [], // Tableau pour [date début, date fin]
+    desiredDate: [],
     name: "",
+    lastName: "",
     phone: "",
     email: "",
     address: "",
 });
 
-// Gérer la sélection des services
 const handleServiceSelection = (serviceIds: number[]) => {
     selectedServiceIds.value = serviceIds;
     formData.services = [...serviceIds];
 };
 
-// Fonctions de navigation entre les étapes
 const nextStep = () => {
     if (step.value < 3) step.value++;
 };
@@ -49,46 +44,43 @@ const prevStep = () => {
     if (step.value > 1) step.value--;
 };
 
-// Fonction appelée au clic sur "Valider"
 const submitForm = () => {
     emit("submit", formData);
 };
 </script>
 
 <template>
-    <!-- Modal principal -->
     <VueFinalModal
         overlay-transition="vfm-fade"
         content-transition="vfm-scale-fade"
         overlay-class="bg-[rgba(0,0,0,0.4)]"
         class="flex justify-center items-center px-2"
-        content-class="w-full max-w-5xl rounded-lg shadow-lg relative max-h-[90vh] flex flex-col"
+        content-class="w-full max-w-5xl rounded-lg shadow-lg relative h-[min(720px,90vh)] flex flex-col"
         :reserve-scroll-bar-gap="false"
         :lock-scroll="false"
     >
-        <!-- Zone de contenu défilable -->
         <div
-            class="flex-1 overflow-y-auto p-6 rounded-t-lg"
+            class="flex-1 overflow-y-auto rounded-t-lg flex justify-center"
             :class="{
                 'bg-[#FBFAF6]': step === 1 || step === 3,
                 'bg-[#2D2D2D]': step === 2,
             }"
         >
-            <!-- Étape 1 : Choix du/des service(s) avec le composant Services -->
-            <div v-if="step === 1" class="flex flex-col gap-6">
-                <h3 class="text-[#0D0703] font-medium text-2xl font-poppins">
+            <div
+                v-if="step === 1"
+                class="flex flex-col gap-11 w-full max-w-3xl pt-16"
+            >
+                <h3
+                    class="text-3xl text-[#0D0703] font-poppins font-medium text-center"
+                >
                     Je désire :
                 </h3>
-                <!-- Services avec une hauteur réduite et une marge en bas -->
-                <div class="services-container">
-                    <Services
-                        :selectable="true"
-                        :selected-services="selectedServiceIds"
-                        height="200px"
-                        @service-selected="handleServiceSelection"
-                    />
-                </div>
-                <!-- Affichage du nombre de services sélectionnés -->
+                <Services
+                    :selectable="true"
+                    :selected-services="selectedServiceIds"
+                    height="200px"
+                    @service-selected="handleServiceSelection"
+                />
                 <div
                     v-if="selectedServiceIds.length > 0"
                     class="mt-2 text-sm text-gray-600"
@@ -100,18 +92,22 @@ const submitForm = () => {
                 </div>
             </div>
 
-            <!-- Étape 2 : Description du projet + Datepicker -->
-            <div v-else-if="step === 2" class="flex flex-col gap-6 h-max">
-                <h3 class="text-[#ffff] font-medium text-2xl font-poppins">
+            <div
+                v-else-if="step === 2"
+                class="flex flex-col gap-11 w-full justify-center text-center max-w-3xl"
+            >
+                <h3
+                    class="text-[#ffff] text-3xl font-poppins font-medium text-center"
+                >
                     Dites m'en plus :
                 </h3>
-                <textarea
-                    v-model="formData.description"
-                    rows="4"
-                    placeholder="Décrivez votre projet ou intervention..."
-                    class="w-full h-[180px] bg-white/10 border border-white/20 text-white rounded-[6px] focus:ring-[#FF8C42] focus:border-[#FF8C42] p-2 font-inter text-base"
-                ></textarea>
-                <div>
+                <div class="flex flex-col gap-4">
+                    <textarea
+                        v-model="formData.description"
+                        rows="4"
+                        placeholder="Décrivez votre projet..."
+                        class="w-full h-[180px] bg-white/10 border border-white/20 text-white rounded-[6px] focus:ring-[#FF8C42] focus:border-[#FF8C42] p-2 font-inter text-base"
+                    ></textarea>
                     <Datepicker
                         v-model="formData.desiredDate"
                         range
@@ -119,7 +115,7 @@ const submitForm = () => {
                         text-input
                         auto-apply
                         :format="'dd/MM/yyyy'"
-                        input-class-name="w-max bg-white/10 border border-white/20 rounded-[6px] focus:ring-[#FF8C42] focus:border-[#FF8C42] p-2 text-white"
+                        input-class-name="w-full sm:w-max bg-white/10 border border-white/20 rounded-[6px] focus:ring-[#FF8C42] focus:border-[#FF8C42] p-2 text-white"
                         :theme-color="'#FF8C42'"
                         placeholder="Période souhaitée"
                         :teleport="true"
@@ -127,51 +123,70 @@ const submitForm = () => {
                 </div>
             </div>
 
-            <!-- Étape 3 : Coordonnées du client -->
-            <div v-else-if="step === 3" class="flex flex-col gap-6">
-                <h3 class="text-xl font-semibold">Parlez-moi de vous :</h3>
+            <div
+                v-else-if="step === 3"
+                class="flex flex-col gap-11 w-full max-w-3xl justify-center"
+            >
+                <h3
+                    class="text-3xl text-[#0D0703] font-poppins font-medium text-center"
+                >
+                    Parlez-moi de vous :
+                </h3>
                 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                     <div>
-                        <label class="block mb-1 font-medium">Nom *</label>
+                        <label class="block text-[#2D2D2D] font-inter"
+                            >Nom*</label
+                        >
                         <input
                             type="text"
                             v-model="formData.name"
-                            class="border rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-orange-500"
+                            class="border rounded py-2 px-3 border-white/20 bg-[#0D07030D] w-full focus:outline-none focus:ring-2 focus:ring-orange-500"
                         />
                     </div>
                     <div>
-                        <label class="block mb-1 font-medium"
-                            >Téléphone *</label
+                        <label class="block text-[#2D2D2D] font-inter"
+                            >Prénom*</label
+                        >
+                        <input
+                            type="text"
+                            v-model="formData.lastName"
+                            class="border rounded py-2 px-3 border-white/20 bg-[#0D07030D] w-full focus:outline-none focus:ring-2 focus:ring-orange-500"
+                        />
+                    </div>
+                    <div>
+                        <label class="block text-[#2D2D2D] font-inter"
+                            >Téléphone*</label
                         >
                         <input
                             type="text"
                             v-model="formData.phone"
-                            class="border rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-orange-500"
+                            class="border rounded py-2 px-3 border-white/20 bg-[#0D07030D] w-full focus:outline-none focus:ring-2 focus:ring-orange-500"
                         />
                     </div>
                     <div>
-                        <label class="block mb-1 font-medium">Email *</label>
+                        <label class="block text-[#2D2D2D] font-inter"
+                            >Email*</label
+                        >
                         <input
                             type="email"
                             v-model="formData.email"
-                            class="border rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-orange-500"
+                            class="border rounded py-2 px-3 border-white/20 bg-[#0D07030D] w-full focus:outline-none focus:ring-2 focus:ring-orange-500"
                         />
                     </div>
-                    <div>
-                        <label class="block mb-1 font-medium"
-                            >Adresse (optionnel)</label
+                    <div class="col-span-2">
+                        <label class="block text-[#2D2D2D] font-inter"
+                            >Adresse</label
                         >
-                        <input
-                            type="text"
+                        <textarea
                             v-model="formData.address"
-                            class="border rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-orange-500"
-                        />
+                            rows="3"
+                            class="border rounded-[6px] py-2 px-3 border-white/20 bg-[#0D07030D] w-full focus:outline-none focus:ring-2 focus:ring-orange-500 h-[100px] max-h-[100px]"
+                        ></textarea>
                     </div>
                 </div>
             </div>
         </div>
 
-        <!-- Barre de boutons fixe en bas du modal -->
         <div
             class="p-6 rounded-b-lg sticky bottom-0"
             :class="{
@@ -229,7 +244,6 @@ const submitForm = () => {
 </template>
 
 <style scoped>
-/* Transitions overlay */
 .vfm-fade-enter-active,
 .vfm-fade-leave-active {
     transition: opacity 0.3s ease;
@@ -239,7 +253,6 @@ const submitForm = () => {
     opacity: 0;
 }
 
-/* Transition du contenu : scale & fade */
 .vfm-scale-fade-enter-active,
 .vfm-scale-fade-leave-active {
     transition: opacity 0.4s cubic-bezier(0.25, 0.1, 0.25, 1),
