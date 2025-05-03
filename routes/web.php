63707cef4987408192577a9ae40c9936bbd67d07
Artyom-exe <thlambot@hotmail.fr>
2025-05-03 15:35:03 +0200

    Suppression de la route pour afficher un devis et nettoyage du code associé dans le contrôleur des devis.

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,6 @@ Route::middleware([
 
     // Routes pour les devis
     Route::get('/devis', [QuoteController::class, 'index'])->name('devis');
-    Route::get('/devis/{quote}', [QuoteController::class, 'show'])->name('devis.show');
     Route::delete('/devis/{quote}', [QuoteController::class, 'destroy'])->name('devis.destroy');
 });
 
