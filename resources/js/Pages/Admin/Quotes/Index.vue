66442a0792362244cbafed0b5dc4785c9a5d22fc
Artyom-exe <th.lambot@hotmail.fr>
2025-05-05 19:30:38 +0200

    Ajout du champ 'end_date' dans le modèle Quote et mise à jour du contrôleur et des composants pour gérer les dates de fin lors de la création de devis.

--- resources/js/Pages/Admin/Quotes/Index.vue
+++ resources/js/Pages/Admin/Quotes/Index.vue
@@ -9,6 +9,15 @@ const props = defineProps({
     errors: Object,
     flash: Object,
 });
+
+// Fonction pour formater une date au format jour/mois/année
+const formatDate = (dateString) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    return `${String(date.getDate()).padStart(2, "0")}/${String(
+        date.getMonth() + 1
+    ).padStart(2, "0")}/${date.getFullYear()}`;
+};
 </script>
 
 <template>
@@ -85,7 +94,7 @@ const props = defineProps({
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 256 512"
-                                            class="w-4 h-4 fill-current text-[#FF8C42] ml-1"
+                                            class="w-4 h-4 fill-current text-[#FF8C42] ml-1 transition-transform duration-300 group-hover:translate-x-1"
                                         >
                                             <path
                                                 d="M246.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 41.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"
@@ -97,6 +106,131 @@ const props = defineProps({
                             <p class="font-inter text-base font-normal">
                                 {{ quote.description }}
                             </p>
+                            <div
+                                class="flex flex-start content-start gap-6 self-stretch font-inter text-base font-normal"
+                            >
+                                <div class="flex flex-col gap-4">
+                                    <div class="flex gap-6">
+                                        <div class="flex items-center gap-3">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="24"
+                                                height="24"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                            >
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    clip-rule="evenodd"
+                                                    d="M17.297 6.57202C16.887 6.57202 16.547 6.23202 16.547 5.82202V4.59802C15.773 4.57202 14.864 4.57202 13.797 4.57202H11.797C10.729 4.57202 9.821 4.57202 9.047 4.59802V5.82202C9.047 6.23202 8.707 6.57202 8.297 6.57202C7.887 6.57202 7.547 6.23202 7.547 5.82202V4.70602C6.602 4.82902 5.953 5.06602 5.497 5.52202C4.895 6.12402 4.675 7.06202 4.594 8.57202H21C20.919 7.06202 20.698 6.12402 20.097 5.52202C19.641 5.06602 18.992 4.82902 18.047 4.70602V5.82202C18.047 6.23202 17.707 6.57202 17.297 6.57202ZM21.041 10.072C21.0463 10.6007 21.0483 11.184 21.047 11.822V12.822C21.047 13.232 21.387 13.572 21.797 13.572C22.207 13.572 22.547 13.232 22.547 12.822V11.822C22.547 7.84202 22.547 5.85202 21.157 4.46202C20.385 3.69002 19.427 3.34702 18.047 3.19402V2.32202C18.047 1.91202 17.707 1.57202 17.297 1.57202C16.887 1.57202 16.547 1.91202 16.547 2.32202V3.09702C15.751 3.07202 14.842 3.07202 13.797 3.07202H11.797C10.751 3.07202 9.843 3.07202 9.047 3.09702V2.32202C9.047 1.91202 8.707 1.57202 8.297 1.57202C7.887 1.57202 7.547 1.91202 7.547 2.32202V3.19402C6.167 3.34702 5.209 3.69002 4.437 4.46202C3.047 5.85202 3.047 7.85202 3.047 11.822V13.822C3.047 17.802 3.047 19.792 4.437 21.182C5.827 22.572 7.817 22.572 11.797 22.572C12.207 22.572 12.547 22.232 12.547 21.822C12.547 21.412 12.207 21.072 11.797 21.072C8.237 21.072 6.447 21.072 5.497 20.122C4.547 19.172 4.547 17.382 4.547 13.822V11.822C4.54633 11.1847 4.548 10.6014 4.552 10.072H21.041ZM17.797 23.072C15.177 23.072 13.047 20.942 13.047 18.322C13.047 15.702 15.177 13.572 17.797 13.572C20.417 13.572 22.547 15.702 22.547 18.322C22.547 20.942 20.417 23.072 17.797 23.072ZM17.797 15.072C16.007 15.072 14.547 16.532 14.547 18.322C14.547 20.112 16.007 21.572 17.797 21.572C19.587 21.572 21.047 20.112 21.047 18.322C21.047 16.532 19.587 15.072 17.797 15.072ZM18.267 19.852C18.417 20.002 18.607 20.072 18.797 20.072C18.987 20.072 19.177 20.002 19.327 19.852C19.617 19.562 19.617 19.082 19.327 18.792L18.547 18.012V16.322C18.547 15.912 18.207 15.572 17.797 15.572C17.387 15.572 17.047 15.912 17.047 16.322V18.322C17.047 18.522 17.127 18.712 17.267 18.852L18.267 19.852Z"
+                                                    fill="white"
+                                                />
+                                            </svg>
+                                            <span>
+                                                {{
+                                                    quote.requested_date !==
+                                                    null
+                                                        ? quote.end_date !==
+                                                          null
+                                                            ? `${formatDate(
+                                                                  quote.requested_date
+                                                              )} - ${formatDate(
+                                                                  quote.end_date
+                                                              )}`
+                                                            : formatDate(
+                                                                  quote.requested_date
+                                                              )
+                                                        : "non renseignée"
+                                                }}
+                                            </span>
+                                        </div>
+                                        <div class="flex items-center gap-3">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="24"
+                                                height="24"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                            >
+                                                <path
+                                                    d="M6.54 5C6.6 5.89 6.75 6.76 6.99 7.59L5.79 8.79C5.38 7.59 5.12 6.32 5.03 5H6.54ZM16.4 17.02C17.25 17.26 18.12 17.41 19 17.47V18.96C17.68 18.87 16.41 18.61 15.2 18.21L16.4 17.02ZM7.5 3H4C3.45 3 3 3.45 3 4C3 13.39 10.61 21 20 21C20.55 21 21 20.55 21 20V16.51C21 15.96 20.55 15.51 20 15.51C18.76 15.51 17.55 15.31 16.43 14.94C16.331 14.903 16.2256 14.886 16.12 14.89C15.86 14.89 15.61 14.99 15.41 15.18L13.21 17.38C10.3755 15.9303 8.06966 13.6245 6.62 10.79L8.82 8.59C9.1 8.31 9.18 7.92 9.07 7.57C8.69132 6.41789 8.4989 5.21274 8.5 4C8.5 3.45 8.05 3 7.5 3Z"
+                                                    fill="white"
+                                                />
+                                            </svg>
+                                            <a
+                                                v-if="
+                                                    quote.client?.phone !== null
+                                                "
+                                                :href="`tel:${quote.client?.phone}`"
+                                                class="hover:text-[#FF8C42] transition-colors duration-300"
+                                            >
+                                                {{ quote.client?.phone }}
+                                            </a>
+                                            <template v-else
+                                                >non renseigné</template
+                                            >
+                                        </div>
+                                    </div>
+                                    <div class="flex gap-6">
+                                        <div class="flex items-center gap-3">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="20"
+                                                height="16"
+                                                viewBox="0 0 20 16"
+                                                fill="none"
+                                            >
+                                                <path
+                                                    d="M2 16C1.45 16 0.979333 15.8043 0.588 15.413C0.196667 15.0217 0.000666667 14.5507 0 14V2C0 1.45 0.196 0.979333 0.588 0.588C0.98 0.196666 1.45067 0.000666667 2 0H18C18.55 0 19.021 0.196 19.413 0.588C19.805 0.98 20.0007 1.45067 20 2V14C20 14.55 19.8043 15.021 19.413 15.413C19.0217 15.805 18.5507 16.0007 18 16H2ZM10 9L2 4V14H18V4L10 9ZM10 7L18 2H2L10 7ZM2 4V2V14V4Z"
+                                                    fill="white"
+                                                />
+                                            </svg>
+                                            <a
+                                                v-if="
+                                                    quote.client?.email !== null
+                                                "
+                                                :href="`mailto:${quote.client?.email}`"
+                                                class="hover:text-[#FF8C42] transition-colors duration-300"
+                                            >
+                                                {{ quote.client?.email }}
+                                            </a>
+                                            <template v-else
+                                                >non renseigné</template
+                                            >
+                                        </div>
+                                        <div class="flex items-center gap-3">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="24"
+                                                height="24"
+                                                fill="currentColor"
+                                                class="bi bi-geo-alt"
+                                                viewBox="0 0 16 16"
+                                            >
+                                                <path
+                                                    d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"
+                                                />
+                                                <path
+                                                    d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"
+                                                />
+                                            </svg>
+                                            <a
+                                                v-if="quote.client?.adress"
+                                                :href="`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                                                    quote.client?.adress
+                                                )}`"
+                                                target="_blank"
+                                                rel="noopener"
+                                                class="font-inter text-blue-300 text-base font-normal hover:text-[#FF8C42] transition-colors cursor-pointer flex items-center gap-2"
+                                                >{{ quote.client?.adress }}</a
+                                            >
+                                            <template v-else
+                                                >non renseigné</template
+                                            >
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
