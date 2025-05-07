75010ffd06e46df4495976a0ee504b6b7eb91b5e
Artyom-exe <thlambot@hotmail.fr>
2025-05-07 13:40:06 +0200

    Améliorer la présentation des détails de l'intervention avec une mise en forme et des classes CSS optimisées

--- resources/js/Pages/Admin/Interventions/Show.vue
+++ resources/js/Pages/Admin/Interventions/Show.vue
@@ -185,17 +185,52 @@ function updateStatus(intervention) {
                                 />
                             </svg>
                             <span class="font-inter text-base font-normal">
-                                <span :class="{'text-gray-400': !intervention.devis.requested_date}">{{ intervention.devis.requested_date ? formatDate(intervention.devis.requested_date) : "Non renseigné" }}</span>
+                                <span
+                                    :class="{
+                                        'text-gray-400':
+                                            !intervention.devis.requested_date,
+                                    }"
+                                    >{{
+                                        intervention.devis.requested_date
+                                            ? formatDate(
+                                                  intervention.devis
+                                                      .requested_date
+                                              )
+                                            : "Non renseigné"
+                                    }}</span
+                                >
                                 -
-                                <span :class="{'text-gray-400': !intervention.devis.end_date}">{{ intervention.devis.end_date ? formatDate(intervention.devis.end_date) : "Non renseigné" }}</span></span
+                                <span
+                                    :class="{
+                                        'text-gray-400':
+                                            !intervention.devis.end_date,
+                                    }"
+                                    >{{
+                                        intervention.devis.end_date
+                                            ? formatDate(
+                                                  intervention.devis.end_date
+                                              )
+                                            : "Non renseigné"
+                                    }}</span
+                                ></span
                             >
                         </div>
                     </div>
                     <div class="flex items-start gap-8 w-full">
-                        <div class="font-inter w-1/2 flex-col items-start text-base font-normal flex gap-2">
+                        <div
+                            class="font-inter w-1/2 flex-col items-start text-base font-normal flex gap-2"
+                        >
                             <a
-                                :href="intervention.client.phone ? `tel:${intervention.client.phone}` : '#'"
-                                :class="intervention.client.phone ? 'text-[#FF8C42] hover:text-white transition-colors cursor-pointer flex gap-2 items-start flex-col' : 'text-gray-400 flex gap-2 items-start flex-col'"
+                                :href="
+                                    intervention.client.phone
+                                        ? `tel:${intervention.client.phone}`
+                                        : '#'
+                                "
+                                :class="
+                                    intervention.client.phone
+                                        ? 'text-[#FF8C42] hover:text-white transition-colors cursor-pointer flex gap-2 items-start flex-col'
+                                        : 'text-gray-400 flex gap-2 items-start flex-col'
+                                "
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -209,13 +244,27 @@ function updateStatus(intervention) {
                                         d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
                                     />
                                 </svg>
-                                {{ intervention.client.phone ? intervention.client.phone : "Non renseigné" }}
+                                {{
+                                    intervention.client.phone
+                                        ? intervention.client.phone
+                                        : "Non renseigné"
+                                }}
                             </a>
                         </div>
-                        <div class="font-inter w-1/2 flex-col items-start text-base font-normal flex gap-2">
+                        <div
+                            class="font-inter w-1/2 flex-col items-start text-base font-normal flex gap-2"
+                        >
                             <a
-                                :href="intervention.client.email ? `mailto:${intervention.client.email}` : '#'"
-                                :class="intervention.client.email ? 'text-white hover:text-[#FF8C42] transition-colors cursor-pointer flex gap-2 items-start flex-col' : 'text-gray-400 flex gap-2 items-start flex-col '"
+                                :href="
+                                    intervention.client.email
+                                        ? `mailto:${intervention.client.email}`
+                                        : '#'
+                                "
+                                :class="
+                                    intervention.client.email
+                                        ? 'text-white hover:text-[#FF8C42] transition-colors cursor-pointer flex gap-2 items-start flex-col'
+                                        : 'text-gray-400 flex gap-2 items-start flex-col '
+                                "
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -229,17 +278,37 @@ function updateStatus(intervention) {
                                         d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"
                                     />
                                 </svg>
-                                {{ intervention.client.email ? intervention.client.email : "Non renseigné" }}
+                                {{
+                                    intervention.client.email
+                                        ? intervention.client.email
+                                        : "Non renseigné"
+                                }}
                             </a>
                         </div>
                     </div>
                     <div class="flex items-start gap-8 w-full">
-                        <div class="font-inter flex-col items-start text-base font-normal flex gap-2 w-full">
+                        <div
+                            class="font-inter flex-col items-start text-base font-normal flex gap-2 w-full"
+                        >
                             <a
-                                :href="intervention.client.adress ? `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(intervention.client.adress)}` : '#'"
-                                :target="intervention.client.adress ? '_blank' : ''"
-                                :rel="intervention.client.adress ? 'noopener' : ''"
-                                :class="intervention.client.adress ? 'text-blue-300 hover:text-[#FF8C42] transition-colors cursor-pointer flex gap-2 items-start flex-col' : 'text-gray-400 flex gap-2 items-start flex-col'"
+                                :href="
+                                    intervention.client.adress
+                                        ? `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                                              intervention.client.adress
+                                          )}`
+                                        : '#'
+                                "
+                                :target="
+                                    intervention.client.adress ? '_blank' : ''
+                                "
+                                :rel="
+                                    intervention.client.adress ? 'noopener' : ''
+                                "
+                                :class="
+                                    intervention.client.adress
+                                        ? 'text-blue-300 hover:text-[#FF8C42] transition-colors cursor-pointer flex gap-2 items-start flex-col'
+                                        : 'text-gray-400 flex gap-2 items-start flex-col'
+                                "
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -256,7 +325,11 @@ function updateStatus(intervention) {
                                         d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"
                                     />
                                 </svg>
-                                {{ intervention.client.adress ? intervention.client.adress : "Non renseigné" }}
+                                {{
+                                    intervention.client.adress
+                                        ? intervention.client.adress
+                                        : "Non renseigné"
+                                }}
                             </a>
                         </div>
                     </div>
@@ -265,63 +338,24 @@ function updateStatus(intervention) {
         </header>
 
         <!-- Contenu principal -->
-        <main class="container mx-auto py-8 px-4">
+        <main class="container">
             <!-- Détails de l'intervention -->
-            <section class="mb-12">
-                <h2 class="text-xl font-bold mb-4">
-                    Détails de l'intervention
-                </h2>
-                <div class="grid grid-cols-3 gap-8">
-                    <!-- Colonne de gauche vide pour l'alignement -->
-                    <div></div>
-
-                    <!-- Colonne du milieu: informations détaillées -->
-                    <div class="bg-gray-100 p-6 rounded-md">
-                        <h3 class="font-semibold mb-2">Service</h3>
-                        <p class="mb-4">
-                            {{
-                                devisDetails?.description ||
-                                "Installation et mise en service"
-                            }}
-                        </p>
-
-                        <div class="space-y-2 mt-4">
-                            <div class="flex">
-                                <span class="text-sm font-semibold"
-                                    >• Type de véhicule:</span
-                                >
-                                <span class="text-sm ml-2">{{
-                                    devisDetails?.vehicleType || "Tesla Model 3"
-                                }}</span>
-                            </div>
-                            <div class="flex">
-                                <span class="text-sm font-semibold"
-                                    >• Prestation souhaitée:</span
-                                >
-                                <span class="text-sm ml-2">{{
-                                    devisDetails?.prestationDuration || "15 min"
-                                }}</span>
-                            </div>
-                            <div class="flex">
-                                <span class="text-sm font-semibold"
-                                    >• Date d'intervention:</span
-                                >
-                                <span class="text-sm ml-2">{{
-                                    formatDate(intervention.date)
-                                }}</span>
-                            </div>
-                            <div class="flex">
-                                <span class="text-sm font-semibold"
-                                    >• Distance au tableau électrique:</span
-                                >
-                                <span class="text-sm ml-2">{{
-                                    devisDetails?.electricalPanel ||
-                                    "Environ 8 mètres"
-                                }}</span>
-                            </div>
-                        </div>
-                    </div>
+            <section class="flex content-center items-start gap-20 py-28 px-16">
+                <div class="relative flex flex-col gap-6">
+                    <h2
+                        class="text-[#0D0703] font-poppins text-[2.5rem] font-medium leading-[120%] tracking-[-.4px]"
+                    >
+                        Détails de l'intervention
+                    </h2>
+                    <div
+                        class="absolute bottom-0 right-[-20%] border-2 border-[#FF8C42] w-[80%] max-w-[353px] min-w-[200px]"
+                    ></div>
                 </div>
+                <p
+                    class="flex min-w-[672px] p-8 flex-col items-start gap-6 rounded-lg border border-white/20 bg-[#F2F2F2p]"
+                >
+                    {{ intervention.devis.description }}
+                </p>
             </section>
 
             <!-- Photos -->
