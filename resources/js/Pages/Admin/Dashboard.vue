f6d72308eda4c477fc4069caa1321a62643b84b9
Artyom-exe <th.lambot@hotmail.fr>
2025-08-07 10:28:44 +0200

    feat: améliorer le style et la réactivité du tableau de bord pour une meilleure expérience utilisateur

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -13,25 +13,29 @@ defineProps({
     <AdminLayout>
         <Head title="Dashboard" />
         <header
-            class="flex py-28 px-16 flex-col items-start gap-20 bg-[#2D2D2D] mt-[3.5rem]"
+            class="flex md:py-28 py-16 md:px-16 px-5 flex-col items-start gap-20 bg-[#2D2D2D] md:mt-[3.5rem] mt-16"
         >
-            <div class="flex items-start gap-20 self-stretch">
+            <div
+                class="flex md:flex-row flex-col items-start md:gap-20 gap-5 self-stretch"
+            >
                 <h1
-                    class="text-white font-poppins text-[2.5rem] font-medium leading-[3rem] w-1/2"
+                    class="text-white font-poppins text-[2rem] md:text-[2.5rem] font-medium md:leading-[3rem] leading-[2.4rem] md:w-1/2"
                 >
                     Suivez l'évolution de vos devis et de vos interventions
                 </h1>
-                <div class="flex flex-col items-start gap-8 flex-1 w-1/2">
-                    <p class="text-white font-inter text-lg">
+                <div class="flex flex-col items-start gap-8 flex-1 md:w-1/2">
+                    <p class="text-white font-inter md:text-lg text-base">
                         Découvrez les statistiques sur les devis en attente et
                         les interventions en cours pour optimiser votre gestion.
                         Restez informé et améliorez vos performances.
                     </p>
                     <div class="flex flex-col items-start gap-4 self-stretch">
-                        <div class="flex py-2 items-start gap-6 self-stretch">
+                        <div
+                            class="flex md:flex-row flex-col py-2 items-start gap-6 self-stretch"
+                        >
                             <div class="flex flex-col items-start gap-2 flex-1">
                                 <span
-                                    class="text-white font-poppins text-[2.5rem] font-medium leading-[3.3rem]"
+                                    class="text-white font-poppins md:text-[2.5rem] text-4xl font-medium md:leading-[3.3rem] leading-[2.7rem]"
                                     >{{ pendingQuotesCount }}</span
                                 >
                                 <p
@@ -42,7 +46,7 @@ defineProps({
                             </div>
                             <div class="flex flex-col items-start gap-2 flex-1">
                                 <span
-                                    class="text-[#FF8C42] font-poppins text-[2.5rem] font-medium leading-[3.3rem]"
+                                    class="text-[#FF8C42] font-poppins md:text-[2.5rem] text-4xl font-medium md:leading-[3.3rem] leading-[2.7rem]"
                                     >{{ ongoingInterventionsCount }}</span
                                 >
                                 <p
@@ -58,15 +62,17 @@ defineProps({
         </header>
 
         <!-- Section des activités récentes -->
-        <section class="bg-white py-28 px-16 items-center flex flex-col gap-20">
+        <section
+            class="bg-white md:py-28 py-16 md:px-16 px-5 items-center flex flex-col md:gap-20 gap-12"
+        >
             <div class="relative flex flex-col gap-6">
                 <h2
-                    class="font-poppins text-5xl text-center font-medium leading-[57.6px] tracking-[-0.48px] mb-6"
+                    class="font-poppins md:text-5xl text-4xl text-center font-medium md:leading-[57.6px] md:tracking-[-0.48px] leading-[43.2px] tracking-[-0.36px] mb-6"
                 >
                     Activités Récentes
                 </h2>
                 <div
-                    class="absolute bottom-0 right-1/2 border-2 border-[#FF8C42] w-[70%] max-w-[353px] min-w-[200px]"
+                    class="absolute md:bottom-0 bottom-2 right-1/2 border-2 border-[#FF8C42] w-[70%] max-w-[353px] md:min-w-[200px] min-w-[188px]"
                 ></div>
             </div>
             <div class="bg-white w-full">
