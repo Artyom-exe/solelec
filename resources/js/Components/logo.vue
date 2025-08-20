2e0b131a264f5662a7d07e14214b6d0eec0c1051
Artyom-exe <th.lambot@hotmail.fr>
2025-08-20 09:06:28 +0200

    fix: remplacer les composants logo par des liens dans les barres de navigation

--- resources/js/Components/logo.vue
+++ resources/js/Components/logo.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+// Pas besoin d'importer Link car nous n'utilisons plus de lien interne
 </script>
 
 <template>
-    <Link href="/" class="flex items-center">
-        <span class="font-inter font-extrabold text-xl text-white leading-7">S<span class="text-[#FF8C42]">O</span>LELEC</span>
-    </Link>
+    <div class="flex items-center">
+        <span class="font-inter font-extrabold text-xl text-white leading-7"
+            >S<span class="text-[#FF8C42]">O</span>LELEC</span
+        >
+    </div>
 </template>
