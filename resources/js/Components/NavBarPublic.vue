710ac6cea7f609901fec9a04e406c42e700ff47a
Artyom-exe <thlambot@hotmail.fr>
2025-09-24 16:17:06 +0200

    fix: mise à jour de la hauteur de la barre de navigation et du contact email sur la page d'accueil

--- resources/js/Components/NavBarPublic.vue
+++ resources/js/Components/NavBarPublic.vue
@@ -361,7 +361,7 @@ const emit = defineEmits(["scrollToSection"]);
 
 <template>
     <nav
-        class="flex fixed top-0 left-0 right-0 bg-[#2D2D2D] md:px-16 px-5 justify-center flex-col items-center md:h-[72px] h-[64px] z-50"
+        class="flex fixed top-0 left-0 right-0 bg-[#2D2D2D] md:px-16 px-5 justify-center flex-col items-center md:h-[72px] h-[66px] z-50"
     >
         <div
             class="flex justify-between items-center md:gap-8 self-stretch w-full max-w-screen-2xl mx-auto"
